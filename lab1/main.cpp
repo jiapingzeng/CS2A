f101722c9e7258ca6db5f5c40b323f884033b0a2
Jiaping Zeng <jiaping@gisar.me>
2018-04-12 17:25:21 -0700

    done

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-    cout << "Hello, World!" << endl;
+    cout << "Hello!" << endl;
+    cout << "My family name is Zeng" << endl;
+    cout << "My student ID is 20275238" << endl;
+    cout << "I noticed in the syllabus that discussions are worth as much as exams, so participating in them will be important for me to succeed in this class." << endl;
+    cout << "I also noticed that late work is not accepted so turning in work on time will be crucial." << endl;
+    cout << "According to homework requirements, I am only allowed to submit one file as attachment." << endl;
+    cout << "Only plain text file is accepted, so I won't need to submit my source code (.cpp files)." << endl;
+    cout << "Tabs are not allowed; I generally use tabs in my code so I will need to keep this in mind." << endl;
     return 0;
 }
