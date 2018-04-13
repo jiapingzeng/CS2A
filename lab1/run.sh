bd2a9389460c242fafca9fb6045828eb2e5f3b7b
Jiaping Zeng <jiaping@gisar.me>
2018-04-12 17:34:39 -0700

    minor text fixes

--- lab1/run.sh
+++ lab1/run.sh
@@ -1,2 +1,2 @@
 # replaces tabs with 4 spaces and generates an txt file for submission
-sed -i 's/\t/    /g' main.cpp && cat main.cpp > output.txt && echo "\n-----------RUN-----------\n" >> output.txt && g++ main.cpp && ./a.out >> output.txt && echo "\n----------------------\n" >> output.txt && cat output.txt
+sed 's/\t/    /g' main.cpp > output.txt && echo "\n----------RUN----------\n" >> output.txt && g++ main.cpp && ./a.out >> output.txt && echo "\n-----------------------\n" >> output.txt && cat output.txt
