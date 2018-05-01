c83a6f2db4c001890f621aece5bd62126d281892
Jiaping Zeng <jiaping@gisar.me>
2018-04-30 21:10:02 -0700

    hope this works

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -3,79 +3,90 @@
 #include <sstream>
 using namespace std;
 
-string recipename;
-double totalSolFiber, totalCals;
+string recipeName;
+double totalCalories, totalDietFiber, totalStarch, totalFat, totalProtein;
 
-// food #1 constants
-const string FOOD_1_NAME = "avacado";
-const int FOOD_1_CALORIES_P100G = 160;
-const double FOOD_1_SOL_FIBER_P100G = 1.75;
+const string INDENT = "    ";
 
-// food #2 constants
-const string FOOD_2_NAME = "tomato";
-const int FOOD_2_CALORIES_P100G = 18;
-const double FOOD_2_SOL_FIBER_P100G = 0.12;
+class Food {
+    public:
+        string name;
+        // per 100g
+        double calories, dietFiber, starch, fat, protein;
+        Food(string, double, double, double, double, double);
+};
 
-// food #3 constants
-const string FOOD_3_NAME = "buffalo mozzarella";
-const int FOOD_3_CALORIES_P100G = 282;
-const double FOOD_3_SOL_FIBER_P100G = 0;
+Food::Food(string name, double calories, double dietFiber, double starch, double fat, double protein) {
+    this->name = name;
+    this->calories = calories;
+    this->dietFiber = dietFiber;
+    this->starch = starch;
+    this->fat = fat;
+    this->protein = protein;
+}
 
-const string INDENT = "   ";
+int getUserInput(Food food) {
+    string userInputStr;
+    int userInputInt;
+    cout << "How many grams of " << food.name << "? ";
+    getline(cin, userInputStr);
+    istringstream(userInputStr) >> userInputInt;
+    return userInputInt;
+}
 
-int updateNutritions(string userInputStr, double foodCals, double foodSolFiber) {
-	int userInputInt;
-	istringstream(userInputStr) >> userInputInt;
-	if (userInputInt < 0) {
-		cout << "Not sure how to make negative food. Please try again.";
-		return 1;
-	} else if (userInputInt > 1500) {
-		cout << "Too much food! Please try again with less.";
-		return 1;
-	}
-	totalCals += userInputInt * (foodCals / 100);
-	totalSolFiber += userInputInt * (foodSolFiber / 100);
-	return 0;
+int updateNutritions(Food food, int grams) {
+    if (grams < 0) {
+        cout << "Not sure how to make negative food. Please try again." << endl;
+        return 1;
+    } else if (grams > 1500) {
+        cout << "Too much food! Please try again with less " << food.name << "." << endl;
+        return 1;
+    }
+    totalCalories += grams * (food.calories / 100);
+    totalDietFiber += grams * (food.dietFiber / 100);
+    totalStarch += grams * (food.starch / 100);
+    totalFat += grams * (food.fat / 100);
+    totalProtein += grams * (food.protein / 100);
+    return 0;
 }
 
 int main() {
+    int userInput;
 
-	string recipeName, userInputStr;
+    // create food information
+    Food food1 ("baked potato", 93.0, 2.2, 17.3, 0.1, 2.5);
+    Food food2 ("raw potato", 77.0, 2.2, 15.4, 0.1, 2.0);
+    Food food3 ("boiled potato", 87.0, 1.8, 14.2, 0.1, 1.9);
+    Food food4 ("canned potato", 62.0, 2.4, 14.7, 0.2, 1.4);
+    Food food5 ("fried potato", 150.0, 1.9, 17.4, 4.7, 2.2);
 
-	// initialize accumulator variables
-	totalSolFiber = 0;
-	totalCals = 0;
+    // print menu
+    cout << "---------- List of Possible Ingredients ----------" << endl;
+    cout << INDENT << "Food #1: " << food1.name << endl;
+    cout << INDENT << "Food #2: " << food2.name << endl;
+    cout << INDENT << "Food #3: " << food3.name << endl;
+    cout << INDENT << "Food #4: " << food4.name << endl;
+    cout << INDENT << "Food #5: " << food5.name << endl;
 
-	// print menu
-	cout << "---------- List of Possible Ingredients ----------" << endl;
-	cout << INDENT << "Food #1: " << FOOD_1_NAME << endl;
-	cout << INDENT << "Food #2: " << FOOD_2_NAME << endl;
-	cout << INDENT << "Food #3: " << FOOD_3_NAME << endl;
+    // name of recipe
+    cout << "What are you calling this recipe? ";
+    getline(cin, recipeName);
 
-	// name of recipe
-	cout << "What are you calling this recipe? ";
-	getline(cin, recipeName);
+    // update total nutrition
+    if (updateNutritions(food1, getUserInput(food1))) return 1;
+    if (updateNutritions(food2, getUserInput(food2))) return 1;
+    if (updateNutritions(food3, getUserInput(food3))) return 1;
+    if (updateNutritions(food4, getUserInput(food4))) return 1;
+    if (updateNutritions(food5, getUserInput(food5))) return 1;
 
-	// food #1
-	cout << "How many grams of " << FOOD_1_NAME << "? ";
-	getline(cin, userInputStr);
-	if (updateNutritions(userInputStr, FOOD_1_CALORIES_P100G, FOOD_1_SOL_FIBER_P100G)) return 1;
+    // report results
+    cout << endl << endl;
+    cout << "Nutrition for " << recipeName << endl;
+    cout << INDENT << "Calories: " << totalCalories << endl;
+    cout << INDENT << "Dietary Fiber: " << totalDietFiber << endl;
+    cout << INDENT << "Starch: " << totalStarch << endl;
+    cout << INDENT << "Fat: " << totalFat << endl;
+    cout << INDENT << "Protein: " << totalProtein << endl; 
 
-	// food #2
-	cout << "How many grams of " << FOOD_2_NAME << "? ";
-	getline(cin, userInputStr);
-	if (updateNutritions(userInputStr, FOOD_2_CALORIES_P100G, FOOD_2_SOL_FIBER_P100G)) return 1;
-
-	// food #3
-	cout << "How many grams of " << FOOD_3_NAME << "? ";
-	getline(cin, userInputStr);
-	if (updateNutritions(userInputStr, FOOD_3_CALORIES_P100G, FOOD_3_SOL_FIBER_P100G)) return 1;
-
-	// report results
-	cout << "----------" << "Nutrition for "
-		<< recipeName << "----------";
-	cout << INDENT << "Calories: " << totalCals << endl;
-	cout << INDENT << "Soluble Fiber: " << totalSolFiber << " grams\n";
-
-	return 0;
+    return 0;
 }
