f82e6d87ff5f7868496ec77106415732928ff98f
Jiaping Zeng <jiaping@gisar.me>
2018-06-01 01:07:41 -0700

    wip

---
+++ lab8/main.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <string.h>
+#include <ctype.h>
+using namespace std;
+
+class DateProfile {
+    private:
+        char gender;
+        char searchGender;
+        int romance;
+        int finance;
+        string name;
+    public:
+        static const int MIN_ROMANCE = 1;
+        static const int MAX_ROMANCE = 10;
+        static const int MIN_FINANCE = 1;
+        static const int MAX_FINANCE = 10;
+        static const int MIN_NAME_LEN = 5;
+        static const int MAX_NAME_LEN = 30;
+        DateProfile();
+        DateProfile(char gender, char searchGender, int romance,
+                    int finance, string name);
+        char getGender();
+        char getSearchGender();
+        int getRomance();
+        int getFinance();
+        string getName();
+        void setGender(char gender);
+        void setSearchGender(char searchGender);
+        void setRomance(int romance);
+        void setFinance(int finance);
+        void setName(string name);
+};
+
+DateProfile::DateProfile() {
+    
+}
+
+DateProfile::DateProfile(char gender, char searchGender, int romance, 
+                            int finance, string name) {
+    setGender(gender);
+    setSearchGender(searchGender);
+    setRomance(romance);
+    setFinance(finance);
+    setName(name);
+}
+
+char DateProfile::getGender() {
+    return gender;
+}
+
+char DateProfile::getSearchGender() {
+    return searchGender;
+}
+
+int DateProfile::getRomance() {
+    return romance;
+}
+
+int DateProfile::getFinance() {
+    return finance;
+}
+
+string DateProfile::getName() {
+    return name;
+}
+
+void DateProfile::setGender(char gender) {
+    gender = toupper(gender);
+    if (gender == 'M' || gender == 'F') {
+        this->gender = gender;
+    }
+}
+
+void DateProfile::setSearchGender(char searchGender) {
+    searchGender = toupper(searchGender);
+    if (searchGender == 'M' || searchGender == 'F') {
+        this->searchGender = searchGender;
+    }
+}
+
+void DateProfile::setRomance(int romance) {
+    if (romance >= MIN_ROMANCE && romance <= MAX_ROMANCE) {
+        this->romance = romance;
+    }
+}
+
+void DateProfile::setFinance(int finance) {
+    if (finance >= MIN_FINANCE && finance <= MAX_FINANCE) {
+        this->finance = finance;
+    }
+}
+
+void DateProfile::setName(string name) {
+    if (name.length() >= MIN_NAME_LEN && name.length() <= MAX_NAME_LEN) {
+        this->name = name;
+    }
+}
