0dc78b088e3c471d56dbf050bfe7add9fa07456a
Jiaping Zeng <jiaping@gisar.me>
2018-06-05 23:02:21 -0700

    progress

--- lab8/main.cpp
+++ lab8/main.cpp
@@ -1,47 +1,63 @@
 #include <iostream>
 #include <string.h>
 #include <ctype.h>
+#include <cmath.h>
 using namespace std;
 
 class DateProfile {
     public:
         // range limits
-        static const int MIN_ROMANCE = 1;
-        static const int MAX_ROMANCE = 10;
-        static const int MIN_FINANCE = 1;
-        static const int MAX_FINANCE = 10;
-        static const int MIN_NAME_LEN = 5;
-        static const int MAX_NAME_LEN = 30;
+        static const int MIN_ROMANCE, MAX_ROMANCE, MIN_FINANCE,
+                        MAX_FINANCE, MIN_NAME_LEN, MAX_NAME_LEN;
         // default values
-        static const char DEFAULT_GEND = 'M';
-        static const char DEFAULT_SEARCH_GEND = 'F';
-        static const int DEFAULT_ROMANCE = 5;
-        static const int DEFAULT_FINANCE = 5;
-        static const string DEFAULT_NAME = "Anonymous";
+        static const char DEFAULT_GENDER, DEFAULT_SEARCH_GENDER;
+        static const int DEFAULT_ROMANCE, DEFAULT_FINANCE;
+        static const string DEFAULT_NAME;
+        // default constructor
         DateProfile();
+        // parametereized constructor
         DateProfile(char gender, char searchGender, int romance,
                     int finance, string name);
+        // accessors
         char getGender();
         char getSearchGender();
         int getRomance();
         int getFinance();
         string getName();
+        // mutators
         void setGender(char gender);
         void setSearchGender(char searchGender);
         void setRomance(int romance);
         void setFinance(int finance);
         void setName(string name);
-        
+        // other functions
+        double fitValue(DateProfile partner);
+        double determineGenderFit(DateProfile partner);
+        double determineRomanceFit(DateProfile partner);
+        double determineFinanceFit(DateProfile partner);
     private:
-        char gender;
-        char searchGender;
-        int romance;
-        int finance;
+        // instance variables
+        char gender, searchGender;
+        int romance, finance;
         string name;
 };
 
+// initialize static member
+const int DateProfile::MIN_ROMANCE = 1;
+const int DateProfile::MAX_ROMANCE = 10;
+const int DateProfile::MIN_FINANCE = 1;
+const int DateProfile::MAX_FINANCE = 10;
+const int DateProfile::MIN_NAME_LEN = 5;
+const int DateProfile::MAX_NAME_LEN = 30;
+const char DateProfile::DEFAULT_GENDER = 'M';
+const char DateProfile::DEFAULT_SEARCH_GENDER = 'F';
+const int DateProfile::DEFAULT_ROMANCE = 5;
+const int DateProfile::DEFAULT_FINANCE = 5;
+const string DateProfile::DEFAULT_NAME = "Anonymous";
+
 DateProfile::DateProfile() {
-    
+    DateProfile(DEFAULT_GENDER, DEFAULT_SEARCH_GENDER, DEFAULT_ROMANCE,
+                DEFAULT_FINANCE, DEFAULT_NAME);
 }
 
 DateProfile::DateProfile(char gender, char searchGender, int romance, 
@@ -77,6 +93,8 @@ void DateProfile::setGender(char gender) {
     gender = toupper(gender);
     if (gender == 'M' || gender == 'F') {
         this->gender = gender;
+    } else {
+        this->gender = DEFAULT_GENDER;
     }
 }
 
@@ -84,23 +102,53 @@ void DateProfile::setSearchGender(char searchGender) {
     searchGender = toupper(searchGender);
     if (searchGender == 'M' || searchGender == 'F') {
         this->searchGender = searchGender;
+    } else {
+        this->searchGender = DEFAULT_SEARCH_GENDER;
     }
 }
 
 void DateProfile::setRomance(int romance) {
     if (romance >= MIN_ROMANCE && romance <= MAX_ROMANCE) {
         this->romance = romance;
+    } else {
+        this->romance = DEFAULT_ROMANCE;
     }
 }
 
 void DateProfile::setFinance(int finance) {
     if (finance >= MIN_FINANCE && finance <= MAX_FINANCE) {
         this->finance = finance;
+    } else {
+        this->finance = DEFAULT_FINANCE;
     }
 }
 
 void DateProfile::setName(string name) {
     if (name.length() >= MIN_NAME_LEN && name.length() <= MAX_NAME_LEN) {
         this->name = name;
+    } else {
+        this->name = DEFAULT_NAME;
     }
+}
+
+double DateProfile::fitValue(DateProfile partner) {
+
+}
+
+double DateProfile::determineGenderFit(DateProfile partner) {
+    return this->gender == partner.getSearchGender() && 
+            this->searchGender == partner.getGender();
+}
+
+double DateProfile::determineRomanceFit(DateProfile partner) {
+
+}
+
+double DateProfile::determineFinanceFit(DateProfile partner) {
+
+}
+
+int main() {
+    cout << "Hello world" << endl;
+    return 0;
 }
