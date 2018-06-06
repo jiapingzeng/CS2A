0917122b464d254955f9a7e06c7cbd8505197478
Jiaping Zeng <jiaping@gisar.me>
2018-06-06 02:38:58 -0700

    done

--- lab6/run.sh
+++ lab6/run.sh
@@ -1,7 +1,7 @@
 # replaces tabs with 4 spaces and generates an txt file for submission
 sed 's/\t/    /g' main.cpp > output.txt
 echo "\n----------RUN----------\n" >> output.txt
-g++ main.cpp -std=c++11
+g++ main.cpp
 ./a.out >> output.txt
 echo "\n-----------------------\n" >> output.txt
 cat output.txt
